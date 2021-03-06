ac5c6ccf4d3aa7d4ea62d7b8253f06a4a1b72cec
hossensumon071 <hossensumon071@gmail.com>
2021-03-06 12:02:46 +0600

    4th commit

--- index.php
+++ index.php
@@ -1,20 +1,6 @@
 <?php get_header(); ?>
 <body <?php body_class() ?>>
-    <!-- Header -->
-    <div class="header">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <h3 class="tagline">
-                    <?php bloginfo("description"); ?>
-                    </h3>
-                    <h1 class="align-self-center display-1 text-center heading">
-                        <?php bloginfo("name"); ?>
-                    </h1>
-                </div>
-            </div>
-        </div>
-    </div>
+ <?php get_template_part("hero"); ?>
     <!-- Post Section -->
     <div class="posts">
         <?php 
